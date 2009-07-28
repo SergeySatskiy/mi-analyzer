cade196903c118ba00bbfee3b65683d23d745a9d
sergey.satskiy@gmail.com <sergey.satskiy@gmail.com@5e116043-b7f9-6478-5cb9-6a0aca7e768b>
2009-07-28 04:48:11 +0000

    Initial revision

---
+++ trunk/mi/mi.cpp
@@ -0,0 +1,333 @@
+
+#include <dlfcn.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdarg.h>
+#include <string.h>
+#include <execinfo.h>
+#include <cxxabi.h>
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+#include <pthread.h>
+
+typedef int (*mutex_function)( pthread_mutex_t * );
+
+
+const char *    defaultLibpthreadPath = "/lib/libpthread.so.0";
+const char *    defaultLogfile = "mi.log";
+
+
+pthread_mutex_t     outputLock = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t     inLock = PTHREAD_MUTEX_INITIALIZER;
+
+
+
+class PthreadWrapper
+{
+    private:
+        void *              handle;
+        FILE *              outputFile;
+        mutex_function      lockFunction;
+        mutex_function      unlockFunction;
+        mutex_function      trylockFunction;
+        bool                putStackTrace;
+        vector<pthread_t>   inProcess;
+
+    public:
+        bool  isProcessed( void ) const
+        {
+            bool found;
+            this->lockFunction( &inLock );
+            found = inProcess.end() != find( inProcess.begin(), inProcess.end(),
+                                             pthread_self() );
+            this->unlockFunction( &inLock );
+            return found;
+        }
+
+        void  registerThread( void )
+        {
+            this->lockFunction( &inLock );
+            inProcess.push_back( pthread_self() );
+            this->unlockFunction( &inLock );
+        }
+
+        void  unregisterThread( void )
+        {
+            this->lockFunction( &inLock );
+            vector<pthread_t>::iterator     candidate( find( inProcess.begin(),
+                                                             inProcess.end(), pthread_self() ) );
+            if ( candidate != inProcess.end() ) inProcess.erase( candidate );
+            this->unlockFunction( &inLock );
+        }
+
+        PthreadWrapper() :
+            handle( 0 ), outputFile( 0 ), putStackTrace( false )
+        {
+            char *          libpthreadPath( getenv( "MI_LIBPTHREAD" ) );
+            char *          logfilePath( getenv( "MI_LOGFILE" ) );
+            char *          options( getenv( "MI_OPTIONS" ) );
+            const char *    err;
+
+            if ( libpthreadPath == 0 )
+                handle = dlopen( defaultLibpthreadPath, RTLD_LAZY );
+            else
+                handle = dlopen( libpthreadPath, RTLD_LAZY );
+
+            if ( !handle )
+            {
+                fprintf( stderr, "Cannot get libpthread.so handle. dlopen: %s\n", dlerror() );
+                exit( 1 );
+            }
+
+            dlerror();
+            lockFunction = (mutex_function)dlsym( handle, "pthread_mutex_lock" );
+            err = dlerror();
+            if ( err )
+            {
+                fclose( outputFile );
+                dlclose( handle );
+                fprintf( stderr, "Cannot get pthread_mutex_lock() pointer. dlsym: %s\n", err );
+                exit( 1 );
+            }
+
+            unlockFunction = (mutex_function)dlsym( handle, "pthread_mutex_unlock" );
+            err = dlerror();
+            if ( err )
+            {
+                fclose( outputFile );
+                dlclose( handle );
+                fprintf( stderr, "Cannot get pthread_mutex_unlock() pointer. dlsym: %s\n", err );
+                exit( 1 );
+            }
+
+            trylockFunction = (mutex_function)dlsym( handle, "pthread_mutex_trylock" );
+            err = dlerror();
+            if ( err )
+            {
+                fclose( outputFile );
+                dlclose( handle );
+                fprintf( stderr, "Cannot get pthread_mutex_trylock() pointer. dlsym: %s\n", err );
+                exit( 1 );
+            }
+
+            if ( logfilePath == 0 ) outputFile = fopen( defaultLogfile, "w" );
+            else                    outputFile = fopen( logfilePath, "w" );
+            if ( !outputFile )
+            {
+                dlclose( handle );
+                fprintf( stderr, "Cannot open mi.log" );
+                exit( 1 );
+            }
+
+            if ( logfilePath == 0 ) this->write( "Env: log file: %s\n", defaultLogfile );
+            else                    this->write( "Env: log file: %s\n", logfilePath );
+
+            if ( libpthreadPath == 0 ) this->write( "Env: libpthread.so path: %s\n", defaultLibpthreadPath );
+            else                       this->write( "Env: libpthread.so path: %s\n", libpthreadPath );
+
+            if ( options != 0 && strlen( options ) != 0 )
+            {
+                if ( strcmp( options, "stack" ) == 0 )
+                {
+                    putStackTrace = true;
+                }
+                else
+                {
+                    fprintf( stderr, "Unsupported option '%s' in the MI_OPTIONS "
+                                     "environment variable. Supported values: 'stack'.", options );
+                    exit( 1 );
+                }
+            }
+
+            if ( putStackTrace ) this->write( "Env: print stack trace\n" );
+            else                 this->write( "Env: do not print stack trace\n" );
+        }
+
+        ~PthreadWrapper()
+        {
+            if ( outputFile ) fclose( outputFile );
+            if ( handle )     dlclose( handle );
+        }
+
+        int write( const char * format, ... ) const
+        {
+            int     ret;
+
+            va_list l;
+            va_start( l, format );
+            ret = vfprintf( outputFile, format, l );
+            va_end( l );
+
+            return ret;
+        }
+
+        // Based on: http://idlebox.net/2008/0901-stacktrace-demangled/
+        void saveStack( void )
+        {
+            if ( !putStackTrace )   return;
+
+            this->registerThread();
+
+            // storage array for stack trace address data
+            void *      addrlist[1024];
+
+            // retrieve current stack addresses
+            int         addrlen = backtrace( addrlist, 1024 );
+            if ( addrlen == 0 )
+            {
+                this->write( "Bt:  <empty, possibly corrupt>\n" );
+                return;
+            }
+
+            // resolve addresses into strings containing "filename(function+address)",
+            // this array must be free()-ed
+            char **     symbollist = backtrace_symbols( addrlist, addrlen );
+
+            // allocate string which will be filled with the demangled function name
+            size_t      funcnamesize = 256;
+            char *      funcname = (char*) malloc( funcnamesize );
+
+            // iterate over the returned symbol lines. skip the first, it is the
+            // address of this function.
+            for ( int i = 1; i < addrlen; i++ )
+            {
+                char *  begin_name = 0;
+                char *  begin_offset = 0;
+                char *  end_offset = 0;
+
+                // find parentheses and +address offset surrounding the mangled name:
+                // ./module(function+0x15c) [0x8048a6d]
+                for ( char *  p = symbollist[i]; *p; ++p )
+                {
+                    if ( *p == '(' )    begin_name = p;
+                    else if (*p == '+') begin_offset = p;
+                    else if (*p == ')' && begin_offset)
+                    {
+                        end_offset = p;
+                        break;
+                    }
+                }
+
+                if ( begin_name && begin_offset && end_offset && (begin_name < begin_offset) )
+                {
+                    *begin_name++ = '\0';
+                    *begin_offset++ = '\0';
+                    *end_offset = '\0';
+
+                    // mangled name is now in [begin_name, begin_offset) and caller
+                    // offset in [begin_offset, end_offset). now apply __cxa_demangle():
+
+                    int        status;
+                    char *     ret = abi::__cxa_demangle( begin_name,
+                                                          funcname, &funcnamesize, &status);
+                    if ( status == 0 )
+                    {
+                        funcname = ret; // use possibly realloc()-ed string
+                        this->write( "Bt:  %s : %s+%s\n",
+                                     symbollist[i], funcname, begin_offset );
+                    }
+                    else
+                    {
+                        // demangling failed. Output function name as a C function
+                        // with no arguments.
+                        this->write( "Bt:  %s : %s()+%s\n",
+                                     symbollist[i], begin_name, begin_offset );
+                    }
+                }
+                else
+                {
+                    // couldn't parse the line? print the whole line.
+                    this->write( "Bt:  %s\n", symbollist[i] );
+                }
+            }
+
+            free( funcname );
+            free( symbollist );
+            this->unregisterThread();
+        }
+
+        mutex_function  getLockFunction( void )    const { return lockFunction; }
+        mutex_function  getUnlockFunction( void )  const { return unlockFunction; }
+        mutex_function  getTrylockFunction( void ) const { return trylockFunction; }
+};
+
+
+const clock_t     maxClock( ~0x0 );
+clock_t  clockDiff( clock_t  start, clock_t  end )
+{
+    if ( end >= start ) return end - start;
+    return (maxClock - start) + end;    // Overrun
+}
+
+
+
+static PthreadWrapper   pw;
+
+extern "C"
+{
+    int pthread_mutex_lock( pthread_mutex_t *  m )
+    {
+        if ( pw.isProcessed() )
+        {
+            return pw.getLockFunction()( m );
+        }
+
+        clock_t     before( clock() );
+        int         retVal( pw.getLockFunction()( m ) );
+        clock_t     after( clock() );
+
+        pw.getLockFunction()( &outputLock );
+        pw.write( "Op: lock Object: 0x%p Thread: %lu RetCode: %d Clocks: %lu\n",
+                  m, pthread_self(), retVal, clockDiff( before, after ) );
+        pw.saveStack();
+        pw.getUnlockFunction()( &outputLock );
+
+        return retVal;
+    }
+
+    int pthread_mutex_unlock( pthread_mutex_t *  m )
+    {
+        if ( pw.isProcessed() )
+        {
+            return pw.getUnlockFunction()( m );
+        }
+
+        clock_t     before( clock() );
+        int         retVal( pw.getUnlockFunction()( m ) );
+        clock_t     after( clock() );
+
+        pw.getLockFunction()( &outputLock );
+        pw.write( "Op: unlock Object: 0x%p Thread: %lu RetCode: %d Clocks: %lu\n",
+                  m, pthread_self(), retVal, clockDiff( before, after ) );
+        pw.saveStack();
+        pw.getUnlockFunction()( &outputLock );
+
+        return retVal;
+    }
+
+    int pthread_mutex_trylock( pthread_mutex_t *  m )
+    {
+        if ( pw.isProcessed() )
+        {
+            pw.getTrylockFunction()( m );
+        }
+
+        clock_t     before( clock() );
+        int         retVal( pw.getTrylockFunction()( m ) );
+        clock_t     after( clock() );
+
+        pw.getLockFunction()( &outputLock );
+        pw.write( "Op: trylock Object: 0x%p Thread: %lu RetCode: %d Clocks: %lu\n",
+                  m, pthread_self(), retVal, clockDiff( before, after ) );
+        pw.saveStack();
+        pw.getUnlockFunction()( &outputLock );
+
+        return retVal;
+    }
+}
+
---
+++ trunk/mi/test/drv.cpp
@@ -0,0 +1,7 @@
+int f( void );
+
+int main( int argc, char ** argv )
+{
+    return f();
+}
+
