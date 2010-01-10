22d12afbd04621312a5a393669ca82a160e277d4
sergey.satskiy@gmail.com <sergey.satskiy@gmail.com@5e116043-b7f9-6478-5cb9-6a0aca7e768b>
2010-01-10 04:58:22 +0000

    Tests implementation

--- mi/test/test-bad-unlocking-not-locked-elf.cpp
+++ mi/test/test-bad-unlocking-not-locked-elf.cpp
@@ -1,5 +1,5 @@
 //
-// File:   test-lib-fun-ok.cpp
+// File:   test-bad-unlocking-not-locking-elf.cpp
 //
 // Author: Sergey Satskiy, copyright (c) 2009
 //
@@ -16,53 +16,41 @@
 #include <pthread.h>
 #include <unistd.h>
 
+#include <iostream>
+using namespace std;
+
 pthread_mutex_t     m1 = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t     m2 = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t     m3 = PTHREAD_MUTEX_INITIALIZER;
 
 
 void *  thread0( void * )
 {
-    pthread_mutex_lock( &m1 );
-    pthread_mutex_lock( &m2 );
-    pthread_mutex_lock( &m3 );
-    pthread_mutex_unlock( &m3 );
-    pthread_mutex_unlock( &m2 );
     pthread_mutex_unlock( &m1 );
-
     return 0;
 }
 
 void *  thread1( void * )
 {
-    usleep( 500 );
-    pthread_mutex_lock( &m1 );
-    pthread_mutex_lock( &m2 );
-    pthread_mutex_lock( &m3 );
-    pthread_mutex_unlock( &m3 );
     pthread_mutex_unlock( &m2 );
-    pthread_mutex_unlock( &m1 );
-
     return 0;
 }
 
 
-
-int  f0( void )
+int main( void )
 {
+    cout << "Test (unlocking not locked, elf) t0: m1.unlock" << endl
+         << "                                 t1: m2.unlock" << endl;
+
     pthread_t       t0;
-
-    pthread_create( &t0, 0, thread0, 0 );
-    return 0;
-}
-
-
-
-int  f1( void )
-{
     pthread_t       t1;
 
+    pthread_create( &t0, 0, thread0, 0 );
     pthread_create( &t1, 0, thread1, 0 );
+    pthread_join( t0, 0 );
+    pthread_join( t1, 0 );
+
     return 0;
 }
+
+
 
--- mi/test/test-failed-op.cpp
+++ mi/test/test-failed-op.cpp
@@ -1,5 +1,5 @@
 //
-// File:   test-lib-fun-ok.cpp
+// File:   test-failed-op.cpp
 //
 // Author: Sergey Satskiy, copyright (c) 2009
 //
@@ -16,53 +16,45 @@
 #include <pthread.h>
 #include <unistd.h>
 
+#include <iostream>
+using namespace std;
+
 pthread_mutex_t     m1 = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t     m2 = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t     m3 = PTHREAD_MUTEX_INITIALIZER;
 
 
 void *  thread0( void * )
 {
     pthread_mutex_lock( &m1 );
-    pthread_mutex_lock( &m2 );
-    pthread_mutex_lock( &m3 );
-    pthread_mutex_unlock( &m3 );
-    pthread_mutex_unlock( &m2 );
+    usleep( 500 );
     pthread_mutex_unlock( &m1 );
-
     return 0;
 }
 
 void *  thread1( void * )
 {
-    usleep( 500 );
-    pthread_mutex_lock( &m1 );
-    pthread_mutex_lock( &m2 );
-    pthread_mutex_lock( &m3 );
-    pthread_mutex_unlock( &m3 );
-    pthread_mutex_unlock( &m2 );
-    pthread_mutex_unlock( &m1 );
+    usleep( 100 );
+    if ( pthread_mutex_trylock( &m1 ) == 0 )
+        cout << "Error test execution" << endl;
 
     return 0;
 }
 
 
-
-int  f0( void )
+int main( void )
 {
+    cout << "Test (failed operation, elf) t0: m1.lock -> m1.unlock" << endl
+         << "                             t1: m1.trylock while it is locked" << endl;
+
     pthread_t       t0;
-
-    pthread_create( &t0, 0, thread0, 0 );
-    return 0;
-}
-
-
-
-int  f1( void )
-{
     pthread_t       t1;
 
+    pthread_create( &t0, 0, thread0, 0 );
     pthread_create( &t1, 0, thread1, 0 );
+    pthread_join( t0, 0 );
+    pthread_join( t1, 0 );
+
     return 0;
 }
+
+
 
